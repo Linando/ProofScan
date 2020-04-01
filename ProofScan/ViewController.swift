b74a31dfe2af13477d7ac07293f72f6aee0a0cdf
Linando Saputra <linandosaputra@yahoo.co.id>
2020-04-01 16:15:00 +0700

    Initial Commit

---
+++ ProofScan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProofScan
+//
+//  Created by Linando Saputra on 01/04/20.
+//  Copyright © 2020 Linando Saputra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
